784091ea931ff6c82616bd9033587670d6f2f7ba
RakeshNomula123 <145738461+RakeshNomula123@users.noreply.github.com>
2023-09-25 00:06:19 -0500

    Add files via upload

---
+++ springbootmongo-master/src/main/java/filecompression/FileCompressionApplication.java
@@ -0,0 +1,12 @@
+package filecompression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class FileCompressionApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(FileCompressionApplication.class, args);
+    }
+}
